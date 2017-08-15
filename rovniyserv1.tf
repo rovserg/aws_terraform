91897500b7bd359d9cea3ccd01a9b26b5f608b73
rovserg <30467354+rovserg@users.noreply.github.com>
2017-08-15 18:40:16 +0300

    Update rovniyserv1.tf
    Update machine type

--- rovniyserv1.tf
+++ rovniyserv1.tf
@@ -7,7 +7,7 @@ provider "google" {
 resource "google_compute_instance" "default" {
     
     name         = "lamp"
-    machine_type = "n1-standart-1"
+    machine_type = "n1-standard-1"
     zone         = "us-central1-a"
 
     tags         = ["foo", "bar"]
