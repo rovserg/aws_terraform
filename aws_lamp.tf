942aba124cfe194dd1254f24d29bf4ecb8e2f389
rovserg <rovserg@gmail.com>
2017-08-29 10:27:23 +0000

    ssh key

--- aws_lamp.tf
+++ aws_lamp.tf
@@ -1,10 +1,15 @@
 provider "aws" {
-    access_key = "AKIAICFMNYYD3SKFIN3Q"
-    secret_key = "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"
+    access_key ="AKIAJYPLNITI6QZWUSPQ"
+    secret_key = "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"
     region = "us-west-2"
     }
     
-    resource "aws_instance" "rovserg_lamp" {
-	ami = "ami-9fa343e7"
-	instance_type = "t2.micro"
-	}
+    resource "aws_instance" "rovserg_lamp2" {
+    ami = "ami-0c2aba6c" # CentOS 7 AMI located in US-East-1
+            instance_type = "t2.medium" 
+                    count = "1" # or "10" can be used for parallel creation
+                            vpc_security_group_ids = ["sg-d37022a9"] # some security g
+                            
+                                    key_name = "for lamp" # pre created AWS E2 key pair
+                		    subnet_id = "subnet-f23cd1ba" # AWS VPC subnet 
+                                            }
