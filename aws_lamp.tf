33ee4f7048f747c1d0c24f38c1421d73317fa9ae
rovserg <30467354+rovserg@users.noreply.github.com>
2017-08-30 15:26:09 +0300

    Update aws_lamp.tf
    changed description

--- aws_lamp.tf
+++ aws_lamp.tf
@@ -5,7 +5,7 @@ provider "aws" {
     }
     
     resource "aws_instance" "rovserg_lamp2" {
-    ami = "ami-0c2aba6c" # CentOS 7 AMI located in US-East-1
+    ami = "ami-0c2aba6c" # CentOS 7 AMI located in US-East-2
             instance_type = "t2.medium" 
                     count = "1" # or "10" can be used for parallel creation
                             vpc_security_group_ids = ["sg-d37022a9"] # some security g
